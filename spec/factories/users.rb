4ea90a210cc8bc6bfcb759fbc92f9e0ffd78c921
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 16:50:13 -0300

    feat: adjustment in factory user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { "123456" }
     password_confirmation { "123456" }
-    profile { :admin }
+    profile { %i(admin client).sample }
   end
 end
