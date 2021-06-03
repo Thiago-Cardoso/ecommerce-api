e591cdd4c4da5af3ddc604e79eb711ec66b91f3e
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 16:28:18 -0300

    test:  validation of product category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,6 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   it { is_expected.to belong_to :productable }
+  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
+  it { is_expected.to have_many(:categories).through(:product_categories) }
 end
