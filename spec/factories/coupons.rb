5ee8af2f2d8111477e945ecafda1a34236001f94
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 17:02:47 -0300

    test:  validation of coupons

---
+++ spec/factories/coupons.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :coupon do
+    sequence(:name) { |n| "My Coupon #{n}" }
+    code { Faker::Commerce.unique.promotion_code(digits: 4) }
+    status { %i(active inactive).sample }
+    discount_value { 25 }
+    max_use { 1 }
+    due_date { 3.days.from_now }
+  end
+end
