698c6a5bc64ed86f827afd8fcce9114371ac153d
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 15:21:18 -0300

    test:  validation of product and association

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    sequence(:name) { |n| "Product #{n}" }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
+    productable { nil }
+  end
+end
