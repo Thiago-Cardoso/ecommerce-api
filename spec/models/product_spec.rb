0bfa71550e7305b88c18654768cfb44ed3e5cc8e
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 15:56:20 -0300

    feat: adjustment in models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  subject { build(:product) }
+  
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
--- spec/models/system_requirement_spec.rb
+++ spec/models/system_requirement_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SystemRequirement, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:operational_system) }
   it { is_expected.to validate_presence_of(:storage) }
   it { is_expected.to validate_presence_of(:processor) }
   it { is_expected.to validate_presence_of(:memory) }
   it { is_expected.to validate_presence_of(:video_board) }
+  it { is_expected.to have_many(:games).dependent(:restrict_with_error) }
 end
