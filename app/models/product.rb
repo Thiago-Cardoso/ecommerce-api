4d4a4e8bbfacce5937b6f3ab2a8a47cfea575ba6
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-03 16:27:40 -0300

    feat: create model product category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
-
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+  
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
