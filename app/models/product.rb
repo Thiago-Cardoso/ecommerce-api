7b360e4f100495be45b9ce6dc94552ca54b574a8
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-08 22:38:16 -0300

    feat: add image in product model and adjustment in test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ApplicationRecord
   belongs_to :productable, polymorphic: true
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
-  
+  has_one_attached :image
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :image, presence: true
 end
