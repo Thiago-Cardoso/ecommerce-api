6d07057dc9cc826eb083c6dac8c645ce89a60c83
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-10 20:15:25 -0300

    feat: adjustment in test product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Product, type: :model do
   subject { build(:product) }
   
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
