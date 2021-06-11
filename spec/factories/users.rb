221375f87dedbf2e6b6812699b4e0dfc3bfe5733
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-11 00:19:07 -0300

    test: forbidden access

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { "123456" }
     password_confirmation { "123456" }
-    profile { %i(admin client).sample }
+    profile { :admin }
   end
 end
