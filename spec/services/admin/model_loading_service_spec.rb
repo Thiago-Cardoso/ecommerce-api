dba163351fbf7f86ea046d592d38b23e1325cd11
thiagocardosoti <thiago.cardoso@chiligum.com.br>
2021-06-19 16:56:31 -0300

    refactor: modification of methods in model loading service

--- spec/services/admin/model_loading_service_spec.rb
+++ spec/services/admin/model_loading_service_spec.rb
@@ -1,47 +1,96 @@
-require 'rails_helper'
+
+require "rails_helper"
 
 describe Admin::ModelLoadingService do
-  context 'when #call' do
+  context "when #call" do
     let!(:categories) { create_list(:category, 15) }
 
-    context 'when params are present' do
+    context "when params are present" do
       let!(:search_categories) do
-         categories = []
-         15.times { |n| categories << create(:category, name: "Search #{n + 1}") }
-         categories
+        categories = []
+        15.times { |n| categories << create(:category, name: "Search #{n + 1}") }
+        categories
       end
 
       let(:params) do
-        { search: { name: "Search"}, order: {name: :desc}, page: 2 , length: 4 }
+        { search: { name: "Search" }, order: { name: :desc }, page: 2, length: 4 }
       end
 
-      it 'returns right :length following pagination' do
+      it "performs right :length following pagination" do
         service = described_class.new(Category.all, params)
-        result_categories = service.call
-        expect(result_categories.count).to eq 4
+        service.call
+        expect(service.records.count).to eq 4
       end
 
-      it 'returns records following search, order and pagination' do
-        search_categories.sort! { |a, b| b[:name] <=> a[:name]}
+      it "do right search, order and pagination" do
+        search_categories.sort! { |a, b| b[:name] <=> a[:name] }
         service = described_class.new(Category.all, params)
-        result_categories = service.call
-        expect_categories = search_categories[4..7]
-        expect(result_categories).to contain_exactly *expect_categories
+        service.call
+        expected_categories = search_categories[4..7]
+        expect(service.records).to contain_exactly *expected_categories
+      end
+
+      it "sets right :page" do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:page]).to eq 2
+      end
+
+      it "sets right :length" do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:length]).to eq 4
+      end
+
+      it "sets right :total" do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:total]).to eq 15
+      end
+
+      it "sets right :total_pages" do
+        service = described_class.new(Category.all, params)
+        service.call
+        expect(service.pagination[:total_pages]).to eq 4
       end
     end
 
-    context 'when params are not present' do
-      it 'returns default :length pagination' do
+    context "when params are not present" do
+      it "returns default :length pagination" do
         service = described_class.new(Category.all, nil)
-        result_categories = service.call
-        expect(result_categories.count).to eq 10
+        service.call
+        expect(service.records.count).to eq 10
       end
 
-      it 'returns first 10 records' do
+      it "returns first 10 records" do
         service = described_class.new(Category.all, nil)
-        result_categories = service.call
-        expect_categories = categories[0..9]
-        expect(result_categories).to contain_exactly *expect_categories
+        service.call
+        expected_categories = categories[0..9]
+        expect(service.records).to contain_exactly *expected_categories
+      end
+
+      it "sets right :page" do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:page]).to eq 1
+      end
+
+      it "sets right :length" do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:length]).to eq 10
+      end
+
+      it "sets right :total" do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:total]).to eq 15
+      end
+
+      it "sets right :total_pages" do
+        service = described_class.new(Category.all, nil)
+        service.call
+        expect(service.pagination[:total_pages]).to eq 2
       end
     end
   end
