412f494914ba4d1bd8f6226e1e58bece570065e8
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-20 22:37:39 -0300

    test: add validation featured and adjustment in admin request

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :featured, presence: true, if: -> { featured.nil? }
   validates :image, presence: true
   validates :status, presence: true
   enum status: { available: 1, unavailable: 2 }
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   subject { build(:product) }
-  
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_presence_of(:featured) }
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
   it { is_expected.to belong_to :productable }
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
@@ -16,4 +17,10 @@ RSpec.describe Product, type: :model do
 
   it_behaves_like "name searchable concern", :product
   it_behaves_like "paginatable concern", :product
+
+  it "creates as unfeatured by default" do
+    subject.featured = nil
+    subject.save(validate: false)
+    expect(subject.featured).to be_falsey
+  end
 end
