4e6f578d1ebe9ca3d0136e77091af222529fd775
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-06-20 21:43:19 -0300

    files challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_024758) do
+ActiveRecord::Schema.define(version: 2021_06_20_151136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,16 @@ ActiveRecord::Schema.define(version: 2021_06_16_024758) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["system_requirement_id"], name: "index_games_on_system_requirement_id"
+  end
+
+  create_table "licenses", force: :cascade do |t|
+    t.string "key"
+    t.integer "platform"
+    t.integer "status"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_licenses_on_game_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -121,6 +131,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_024758) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "games", "system_requirements"
+  add_foreign_key "licenses", "games"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe Product, type: :model do
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:categories).through(:product_categories) }
   it { is_expected.to validate_presence_of(:image) }
-  it { is_expected.to define_enum_for(:status).with_values({ available: 1, unavailable: 2 }) }
 
   it_behaves_like "name searchable concern", :product
   it_behaves_like "paginatable concern", :product
