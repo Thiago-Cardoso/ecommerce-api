7382db5b3b414142ecf602cc86733bed97a8490d
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-08-17 22:17:03 -0300

    test: validate of coupon

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -8,17 +8,15 @@ RSpec.describe Coupon, type: :model do
   it { is_expected.to define_enum_for(:status).with_values({ active: 1, inactive: 2 }) }
   it { is_expected.to validate_presence_of :discount_value }
   it { is_expected.to validate_numericality_of(:discount_value).is_greater_than(0) }
-  it { is_expected.to validate_presence_of :max_use }
-  it { is_expected.to validate_numericality_of(:max_use).only_integer.is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of :due_date }
 
-  it "can't have past date due_date" do
+  it "can't have past due_date" do
     subject.due_date = 1.day.ago
     subject.valid?
     expect(subject.errors.keys).to include :due_date
   end
 
-  it "can't with current date due_date" do
+  it "is invalid with current due_date" do
     subject.due_date = Time.zone.now
     subject.valid?
     expect(subject.errors.keys).to include :due_date
@@ -28,6 +26,28 @@ RSpec.describe Coupon, type: :model do
     subject.due_date = Time.zone.now + 1.hour
     subject.valid?
     expect(subject.errors.keys).to_not include :due_date
+  end
+
+  context "on #validate_use!" do
+    subject { build(:coupon) }
+
+    it "raise InvalidUse when it's overdue" do
+      subject.due_date = 2.days.ago
+      expect do
+        subject.validate_use!
+      end.to raise_error(Coupon::InvalidUse)
+    end
+
+    it "raise InvalidUse when it's inactive" do
+      subject.status = :inactive
+      expect do
+        subject.validate_use!
+      end.to raise_error(Coupon::InvalidUse)
+    end
+
+    it "returns true when it's on date and active" do
+      expect(subject.validate_use!).to eq true
+    end
   end
 
   it_behaves_like "like searchable concern", :coupon, :name
