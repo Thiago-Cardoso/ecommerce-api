36e778a6893d680c115ecfc0246f4856f7861dfe
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-08-17 22:46:26 -0300

    test: endpoint coupon

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :coupon do
     sequence(:name) { |n| "My Coupon #{n}" }
     code { Faker::Commerce.unique.promotion_code(digits: 4) }
-    status { %i(active inactive).sample }
+    status { :active }
     discount_value { 25 }
     max_use { 1 }
     due_date { 3.days.from_now }
