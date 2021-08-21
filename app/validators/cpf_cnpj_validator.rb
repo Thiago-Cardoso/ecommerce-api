d58606e2a7197828333376fd3d1a81899d325dd4
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-08-21 16:51:56 -0300

    feat: validation of cnpj and cpf

---
+++ app/validators/cpf_cnpj_validator.rb
@@ -0,0 +1,8 @@
+class CpfCnpjValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return unless value.present?
+    unless CPF.valid?(value) || CNPJ.valid?(value)
+      record.errors.add(attribute, :invalid_cpf_cnpj)
+    end
+  end
+end
